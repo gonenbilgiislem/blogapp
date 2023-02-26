7260ca5ebe1bd58aa33d789a955652d9cfc9d5df
gonenbilgiislem <bilgiislem@balikesirgonen.bel.tr>
2023-02-27 00:19:37 +0300

    .

--- blog/urls.py
+++ blog/urls.py
@@ -1,15 +1,14 @@
 from django.urls import path
 from blog import views
-"""
-Hadicanımsende url yi değiştirebilirsiniz.
-"""
 urlpatterns = [
-    path('', views.index,name="index"),
+    path("hadicanımsende", views.index,name="index"),
+    path('', views.index,name="home"),
     path("index", views.index,name="index"),
     path("blogs", views.blogs,name="blogs"),
     path("blog", views.blogs,name="blogs"),
     path("blog/", views.blogs,name="blogs"),
     path("blogs/", views.blogs,name="blogs"),
     path("blogs/<int:id>", views.blog_details,name="blog_details"),
-    path("hadicanımsende", views.index,name="index")
+    path("admin",views.admin ,name="admin")
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,66 @@
-from django.http.response import HttpResponse
 from django.shortcuts import render
 
-
-# Create your views here.
+data ={
+    "blogs": [
+        {
+            "id": 1,
+            "title": "Komple Web Geliştirme",
+            "image": "1.jpg",
+            "is_active": True,
+            "is_home": False,
+            "description": "çok iyi bir kurs"
+        },
+        {
+            "id": 2,
+            "title": "Python Kursu",
+            "image": "2.jpg",
+            "is_active": True,
+            "is_home": True,
+            "description": "çok iyi bir kurs"
+        },
+        {
+            "id": 3,
+            "title": "Django Kursu",
+            "image": "3.jpg",
+            "is_active": False,
+            "is_home": True,
+            "description": "çok iyi bir kurs"
+        },
+        {
+            "id": 4,
+            "title": "Javascripth Kursu",
+            "image": "4.jpg",
+            "is_active": False,
+            "is_home": False,
+            "description": "çok iyi bir kurs"
+        }
+    ]
+}
 
 def index(request):
-    return render(request, 'blog/index.html')
+    context = {
+        "blogs": data["blogs"],
+        "title": "Anasayfa"
+    }
+    return render(request, 'blog/index.html', context)
+
+def admin(request):
+    return render(request, 'admin/index.html')
 
 def blogs(request):
-    return render(request, 'blog/blogs.html')
+    context = {
+        "blogs": data["blogs"],
+        "title": "Blog Sayfası"
+    }
+    return render(request, 'blog/blogs.html', context)
 
 def blog_details(request, id):
-    return render(request, 'blog/blog_details.html',{'id': id})
+    # blogs = data["blogs"]
+    # selectedBlog = None
+    #
+    # for blog in blogs:
+    #     if blog["id"] == id:
+    #         selectedBlog = blog
+    blogs = data["blogs"]
+    selectedBlog = [blog for blog in blogs if blog["id"] == id][0]
+    return render(request, 'blog/blog_details.html', {"blog": selectedBlog})
