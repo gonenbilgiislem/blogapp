c7c9ca65e978f5f629ef2f4864ee7387f7c35c35
gonenbilgiislem <bilgiislem@balikesirgonen.bel.tr>
2023-02-28 00:02:24 +0300

    .

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'image', 'description', 'is_active', 'is_home','slug')
+    list_editable = ('is_active', 'is_home')
+    list_filter = ('title', 'image', 'description', 'is_active', 'is_home')
+    search_fields = ('title', 'image', 'description', 'is_active', 'is_home')
+    readonly_fields = ('slug',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug',)
+    # list_editable = ('name',)
+    list_filter = ('name',)
+    search_fields = ('name',)
+    readonly_fields = ('slug',)
 
 from .models import Blog, Category
 
-admin.site.register(Blog)
-admin.site.register(Category)
+admin.site.register(Blog,BlogAdmin)
+admin.site.register(Category,CategoryAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,32 @@
 from django.db import models
+from django.utils.text import slugify
+
 
 # Create your models here.
+
 class Blog(models.Model):
     title = models.CharField(max_length=200)
     image = models.CharField(max_length=50)
     description = models.TextField()
     is_active = models.BooleanField(default=False)
     is_home = models.BooleanField(default=False)
+    slug = models.SlugField(null=False, blank=True, unique=True, db_index=True, editable=False)
 
     def __str__(self):
-        return f"{self.title}, {self.image}, {self.description}, {self.is_active}, {self.is_home}"
+        return f"{self.title}"
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Blog, self).save(*args, **kwargs)
+
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
+    slug = models.SlugField(null=False, blank=True, unique=True, db_index=True, editable=False)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.name}"
